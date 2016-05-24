c0988d886d1b8ecbf739d6049a04733e2007166a
Usman Bashir <usman.bashir@mslgroup.co.uk>
2016-05-24 16:19:48 +0100

    Committing initial projects

---
+++ Shared/ProjectA/Car.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectA
+{
+    public class Car
+    {
+        public void drive()
+        {
+            Console.WriteLine("Car is driving");
+        }
+
+        public void stop()
+        {
+            Console.WriteLine("Car is stopping now");
+        }
+    }
+}
---
+++ Shared/ProjectB/Truck.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectB
+{
+    public class Truck
+    {
+        public void drive()
+        {
+            Console.WriteLine("Truck is driving now");
+        }
+        public void stop()
+        {
+            Console.WriteLine("Truck is now stop");
+        }
+    }
+}
